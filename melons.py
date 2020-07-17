9bc2b2ad4ed5d7f9c040a9e4df1cf84c98fe72ac
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-07-17 19:38:40 +0000

    Get Total methods adjusted for special cases Christmas Melons and International Fee

--- melons.py
+++ melons.py
@@ -1,7 +1,7 @@
 """Classes for melon orders."""
 
 class AbstractMelonOrder():
-    """All melon orders. """
+    """An abstract SuperClass that other melons inherit from. """
     order_type = None   #assigned in sub-class
     tax = None          #assigned in sub-class
 
@@ -12,25 +12,31 @@ class AbstractMelonOrder():
         self.shipped = False
 
 
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+
+        if self.species == 'christmas melon':
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     order_type = 'domestic'
     tax = 0.08
 
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -43,21 +49,24 @@ class InternationalMelonOrder(AbstractMelonOrder):
         super().__init__(species, qty)
         self.country_code = country_code
 
-
     def get_total(self):
-        """Calculate price, including tax."""
+        """Calculate price, including tax and international fee."""
+        total = super().get_total()
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        # International fee for orders of less than 10
+        if self.qty < 10:
+            return total + 3    
+        else:
+            return total
 
-        return total
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+
+if __name__ == '__main__':
+    order0 = InternationalMelonOrder('watermelon', 6, "AUS")
+    order1 = DomesticMelonOrder('christmas melon', 5)
