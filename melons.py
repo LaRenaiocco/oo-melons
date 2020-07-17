fc60ff277502fb66e178ab1d0ed49d425949a702
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-07-17 19:18:34 +0000

    SuperClass Abstract Melons created. Def __init__ adjusted for all classes.

--- melons.py
+++ melons.py
@@ -1,17 +1,23 @@
 """Classes for melon orders."""
 
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
+class AbstractMelonOrder():
+    """All melon orders. """
+    order_type = None   #assigned in sub-class
+    tax = None          #assigned in sub-class
 
     def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
+        """ Initialize melon order attributes."""
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    order_type = 'domestic'
+    tax = 0.08
+
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -27,18 +33,16 @@ class DomesticMelonOrder():
         self.shipped = True
 
 
-class InternationalMelonOrder():
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
+    order_type = "international"
+    tax = 0.17
 
-        self.species = species
-        self.qty = qty
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
+
 
     def get_total(self):
         """Calculate price, including tax."""
